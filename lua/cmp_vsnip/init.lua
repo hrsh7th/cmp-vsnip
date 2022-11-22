18ff44cd4fa873d8c2aa23cb26cdd68e326f9e9c
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2022-11-22 11:58:21 +0900

    Improve symbol handling

--- lua/cmp_vsnip/init.lua
+++ lua/cmp_vsnip/init.lua
@@ -10,25 +10,33 @@ source.is_available = function()
   return vim.g.loaded_vsnip
 end
 
-source.get_keyword_pattern = function()
-  return '\\%([^[:alnum:][:blank:]]\\|\\w\\+\\)'
+source.get_position_encoding_kind = function()
+  return 'utf-8'
 end
 
-source.complete = function(_, request, callback)
+source.get_keyword_pattern = function()
+  return '.'
+end
+
+source.complete = function(self, params, callback)
   local completion_items = {}
+
   for _, item in ipairs(vim.fn['vsnip#get_complete_items'](vim.api.nvim_get_current_buf())) do
     local completion_item = {}
     local user_data = vim.fn.json_decode(item.user_data)
-    completion_item.word = item.word
-    completion_item.label = item.abbr
-    completion_item.insertTextFormat = cmp.lsp.InsertTextFormat.Snippet
-    completion_item.insertText = table.concat(user_data.vsnip.snippet, '\n')
-    completion_item.kind = cmp.lsp.CompletionItemKind.Snippet
-    completion_item.data = {
-      filetype = request.context.filetype,
-      snippet = user_data.vsnip.snippet,
-    }
-    table.insert(completion_items, completion_item)
+    local text_edit = self:_get_text_edit(params, item.word, user_data.vsnip)
+    if text_edit then
+      completion_item.label = item.abbr
+      completion_item.filterText = item.word
+      completion_item.insertTextFormat = cmp.lsp.InsertTextFormat.Snippet
+      completion_item.textEdit = text_edit
+      completion_item.kind = cmp.lsp.CompletionItemKind.Snippet
+      completion_item.data = {
+        filetype = params.context.filetype,
+        snippet = user_data.vsnip.snippet,
+      }
+      table.insert(completion_items, completion_item)
+    end
   end
 
   callback(completion_items)
@@ -47,6 +55,30 @@ source.resolve = function(_, completion_item, callback)
     value = table.concat(documentation, '\n'),
   }
   callback(completion_item)
+end
+
+source._get_text_edit = function(_, params, prefix, snippet)
+  local chars = vim.fn.split(prefix, [[\zs]])
+  local chars_pattern = [[\%(\V]] .. table.concat(chars, [[\m\|\V]]) .. [[\m\)]]
+  local whole_pattern = ([[\<\V%s\m[%s]*$]]):format(chars[1], chars_pattern)
+  local regex = vim.regex(whole_pattern)
+  local s, e = regex:match_str(params.context.cursor_before_line)
+  if not s then
+    return
+  end
+  return {
+    newText = table.concat(snippet.snippet, '\n'),
+    range = {
+      start = {
+        line = params.context.cursor.line,
+        character = s,
+      },
+      ['end'] = {
+        line = params.context.cursor.line,
+        character = e,
+      },
+    },
+  }
 end
 
 return source
